77d2bb7bd499aef588f4914f6511549ea675b83c
Fabien Saporito <fabien.sa@ludus-academie.fr>
2025-03-04 17:42:09 +0100

    MaJ Création de la zone de jeu
    Différents effets et décors mise en place

---
+++ ui_menu_ig.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_quitter_pressed() -> void:
+	get_tree().quit()
+
+func _on_option_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_musique_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
