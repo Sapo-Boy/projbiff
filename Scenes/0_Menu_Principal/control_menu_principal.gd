d8f1aa90a8198ac20873a2e84c8aacc9f9482413
Fabien Saporito <fabien.sa@ludus-academie.fr>
2025-03-05 11:36:58 +0100

    MaJ Menu Principal + Instructions

---
+++ Scenes/0_Menu_Principal/control_menu_principal.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/1_Zone_De_Jeu/Scene_De_Jeu.tscn")
+
+func _on_quitter_pressed() -> void:
+	get_tree().quit()
--- ui_menu_ig.gd
+++ ui_menu_ig.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_quitter_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://Scenes/0_Menu_Principal/control_Menu_Principal.tscn")
 
 func _on_option_pressed() -> void:
 	pass # Replace with function body.
