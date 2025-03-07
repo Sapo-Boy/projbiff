325e7a1a85e9d629d10cb6993870e787573ff00c
Fabien Saporito <fabien.sa@ludus-academie.fr>
2025-03-07 13:24:36 +0100

    MaJ retouche du menu option

--- Scenes/ui/MenuOption/menu_option.gd
+++ Scenes/ui/MenuOption/menu_option.gd
@@ -5,6 +5,8 @@ extends Control
 @onready var MUSIQUE_BUS_ID = AudioServer.get_bus_index("Musique")
 @onready var SFX_BUS_ID = AudioServer.get_bus_index("Sfx")
 
+var fullscreen = true
+
 func _on_musique_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(MUSIQUE_BUS_ID, linear_to_db(value))
 	AudioServer.set_bus_mute(MUSIQUE_BUS_ID, value < 0.05)
@@ -15,3 +17,12 @@ func _on_sons_slider_value_changed(value: float) -> void:
 
 func _on_retour_pressed() -> void:
 	bibliothequeSons.SonPresseBouton()
+
+func _on_check_button_pressed() -> void:
+	bibliothequeSons.SonPresseBouton()
+	if fullscreen == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		fullscreen = false
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		fullscreen = true
