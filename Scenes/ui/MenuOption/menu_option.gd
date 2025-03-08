d0ce0617001f2f9c4419160e3c9b1f8a3bc9f436
Fabien Saporito <fabien.sa@ludus-academie.fr>
2025-03-08 20:47:57 +0100

    MaJ

--- Scenes/ui/MenuOption/menu_option.gd
+++ Scenes/ui/MenuOption/menu_option.gd
@@ -5,9 +5,12 @@ extends Control
  
 @onready var MUSIQUE_BUS_ID = AudioServer.get_bus_index("Musique")
 @onready var SFX_BUS_ID = AudioServer.get_bus_index("Sfx")
+
 @onready var plein_ecran_check: CheckButton = $MarginContainer/VBoxContainer/GridContainer2/PleinEcranCheck
+
 @onready var musique_slider: HSlider = $MarginContainer/VBoxContainer/GridContainer/MusiqueSlider
 @onready var sons_slider: HSlider = $MarginContainer/VBoxContainer/GridContainer/SonsSlider
+
 
 func _ready() -> void:
 	#Position par défaut du bouton "Plein écran" / Doit être la même dans l'éditeur ?
@@ -26,6 +29,21 @@ func _ready() -> void:
 	if (vs_setting !=null and vs_setting is float):
 		sons_slider.value = vs_setting
 
+func _on_retour_pressed() -> void:
+	bibliothequeSons.SonPresseBouton()
+	BoutonMenuZdJ.show()
+
+func _on_plein_ecran_check_toggled(toggled_on: bool) -> void:
+	#Stocke la valeur du slider dans "fullscreen" 
+	Settings.set_setting("fullscreen", toggled_on)
+	#Réglage du mode d'écran en fonction de la valeur du bouton
+	if toggled_on:
+		get_window().set_mode(Window.MODE_FULLSCREEN)
+	else:
+		get_window().set_mode(Window.MODE_WINDOWED)
+	#Sauvegarde dans le fichier settings
+	Settings.save_settings()
+	
 func _on_musique_slider_value_changed(value: float) -> void:
 	#Stocke la valeur du slider dans "volumeMusique" 
 	Settings.set_setting("volumeMusique", value)
@@ -43,20 +61,5 @@ func _on_sons_slider_value_changed(value: float) -> void:
 	#Passe en mute si la valeur est <0.5
 	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
 	AudioServer.set_bus_mute(SFX_BUS_ID, value < 0.05)
-	#Sauvegarde dans le fichier settings
-	Settings.save_settings()
-
-func _on_retour_pressed() -> void:
-	bibliothequeSons.SonPresseBouton()
-	BoutonMenuZdJ.show()
-
-func _on_plein_ecran_check_toggled(toggled_on: bool) -> void:
-	#Stocke la valeur du slider dans "volumeSfx" 
-	Settings.set_setting("fullscreen", toggled_on)
-	#Réglage du mode d'écran en fonction de la valeur du bouton
-	if toggled_on:
-		get_window().set_mode(Window.MODE_FULLSCREEN)
-	else:
-		get_window().set_mode(Window.MODE_WINDOWED)
 	#Sauvegarde dans le fichier settings
 	Settings.save_settings()
