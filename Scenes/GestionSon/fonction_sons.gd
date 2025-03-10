0633f4c46a857c8373c65942b3eb6983077096b0
Sapo_Boy <70650786+Sapo-Boy@users.noreply.github.com>
2025-03-10 17:14:54 +0100

    push sons

--- Scenes/GestionSon/fonction_sons.gd
+++ Scenes/GestionSon/fonction_sons.gd
@@ -1,17 +1,16 @@
 extends AudioStreamPlayer
 
 func SonPresseBouton() -> void:
-	play()
 	self["parameters/switch_to_clip"] = "PresseBouton"
+	play()
 
 func SonCarteDepose() -> void:
 	self["parameters/switch_to_clip"] = "CarteDepose"
 	play()
 	
-
 func SonCarteSelection() -> void:
-	play()
 	self["parameters/switch_to_clip"] = "CarteSelection"
+	play()
 
 func SonCarteErreur() -> void:
 	self["parameters/switch_to_clip"] = "CarteErreur"
